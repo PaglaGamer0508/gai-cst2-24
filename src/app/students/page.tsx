fdb418f6306b887262a4befa4f23a19f4cedcdea
fahad <fahadmahmud225@gmail.com>
2024-02-24 12:06:29 +0600

    added students page metadata

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { students } from "@/lib/students";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Students - GAI-CST2-24",
+};
 
 const page: React.FC = () => {
   return (
