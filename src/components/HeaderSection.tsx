70aa43cf1877290b53fa1b0dd582855c949b17e5
fahad <fahadmahmud225@gmail.com>
2024-02-24 23:26:40 +0600

    spacing reduced in HeaderSection

--- src/components/HeaderSection.tsx
+++ src/components/HeaderSection.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const HeaderSection: React.FC = ({}) => {
   return (
-    <div className="min-h-[65vh] grid place-items-center">
-      <div className="sm:w-[80%] md:w-[60%] mx-auto">
+    <div className="min-h-[55vh] grid place-items-center">
+      <div className="max-w-[80%] md:w-[60%] mx-auto">
         <p className="text-center text-xl sm:text-2xl mb-10">
           We are students of{" "}
           <span className="text-blue-500">Govt. Graphic Arts Institute</span>
